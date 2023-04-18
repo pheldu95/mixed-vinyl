b0a230dbe23e48d682e58c2553db5ea449e612eb
pheldu95 <phelps.duncan@gmail.com>
2023-04-18 17:31:06 -0500

    added a wildcard route

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
@@ -11,5 +12,18 @@ class VinylController
     public function homepage(): Response
     {
         return new Response('Title: PB and James');
+    }
+
+    //wildcard route. the $slug = null makes the slug options. so just /browse will still work
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug) {
+            $title = u(str_replace('-', ' ', $slug))->title(true);
+        }else{
+            $title = 'All';
+        }
+        
+        return new Response($title);
     }
 }
