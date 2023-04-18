fb8fb009e697c298cef58ba2f987110862d71d8d
pheldu95 <phelps.duncan@gmail.com>
2023-04-18 17:20:39 -0500

    made our first controller and route

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Title: PB and James');
+    }
+}
