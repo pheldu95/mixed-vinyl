530769a9aee5adfa83de89ca743747c70c91ef0b
pheldu95 <phelps.duncan@gmail.com>
2023-04-19 14:58:08 -0500

    created first twig template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and James');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        //render returns a Response object. controllers must always return a response object
+        return $this->render('vinyl/homepage.html.twig',[
+            'title' => 'PB and Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     //wildcard route. the $slug = null makes the slug options. so just /browse will still work
