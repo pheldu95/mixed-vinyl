ac66a7ae3161b57670d9b478b3ca7b27172255a8
pheldu95 <phelps.duncan@gmail.com>
2023-04-21 11:23:28 -0500

    added LoggerInterface service to getSongs

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ class SongController extends AbstractController
 //    using regex to make sure we get an integer not a string. for the song id in the url
 //      this will throw a 404 error if someone tries to go to /api/songs/apple for example
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -20,6 +21,10 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returing API response for song {song}', [
+            'song' => $id
+        ]);
 //        returns a response that is automatoically json encoded
         return $this->json($song);
     }
