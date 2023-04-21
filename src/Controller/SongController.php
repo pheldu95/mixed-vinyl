e0b9b6383e3c0bf7e586532ad9b46ddba5157705
pheldu95 <phelps.duncan@gmail.com>
2023-04-21 10:48:16 -0500

    added songcontroller. will eventually make api call to get song data from database

---
+++ src/Controller/SongController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id}')]
+    public function getSong($id): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+//        returns a response that is automatoically json encoded
+        return $this->json($song);
+    }
+}
